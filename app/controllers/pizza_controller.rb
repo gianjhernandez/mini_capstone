e4cfd2611e231fda6701cd93a791d88d48c4da2f
Giancarlo Hernandez <gianjhernandez@gmail.com>
2017-11-14 19:27:38 -0600

    display json

--- app/controllers/pizza_controller.rb
+++ app/controllers/pizza_controller.rb
@@ -1,2 +1,7 @@
 class PizzaController < ApplicationController
+  def pizza_method
+    # render json: {pizza_slice: "Jalapeno", pizza_slice2: "Sausage", pizza_slice3: "Giardinera"}
+    pizzas = Pizza.all
+    render json: pizzas.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-pizza_slice = Pizza.new({name: “Jalapeno”, price: "2", image: "", description: "hot"})
-item.save
+# pizza_slice = Pizza.new({name: "Jalapeno", price: "2", image: "", description: "hot"})
+# pizza_slice.save
 
-pizze_slice2 = Pizza.new({name: “Sausage”, price: "3", image: "", description: "meaty"})
-item.save
+pizza_slice2 = Pizza.new({name: "Sausage", price: "3", image: "", description: "meaty"})
+pizza_slice2.save
 
 pizza_slice3 = Pizza.new({name: "Giardinera", price: "3", image: "", description: "tasty"})
-
+pizza_slice3.save
